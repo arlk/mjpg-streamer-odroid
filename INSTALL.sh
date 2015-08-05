57c9ed517424bad7a3c741088300903831ae1f07
Arun Lakshmanan <c.arun.lakshmanan@gmail.com>
2015-08-05 16:03:03 -0500

    Added libv4l compatibility

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,8 +4,7 @@ echo "Installing Pre-reqs"
 sudo apt-get install python python-dev python-numpy python-scipy
 sudo apt-get install python-opencv python-pip
 sudo apt-get install libjpeg-dev libcairo2-dev
-sudo apt-get install v4l-utils
+sudo apt-get install v4l-utils libv4l-dev
 
 echo "Installing mjpg-streamer"
-sudo make clean
-sudo make install
+make USE_LIBV4L2=true clean all
